9a706bf772246a4cc628f52ff6b89848edf142cd
wancong <wancongzhang@gmail.com>
2020-03-29 11:18:45 -0400

    'd'

--- train.py
+++ train.py
@@ -160,7 +160,7 @@ from transformers import get_linear_schedule_with_warmup
 # Number of training epochs. The BERT authors recommend between 2 and 4. 
 # We chose to run for 4, but we'll see later that this may be over-fitting the
 # training data.
-epochs = 4
+epochs = 3
 
 # Total number of training steps is [number of batches] x [number of epochs]. 
 # (Note that this is not the same as the number of training samples).
