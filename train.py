53ff3e9057356a980db0318a05f49f36676aa397
wancong <wancongzhang@gmail.com>
2020-04-09 14:37:41 -0400

    'd'

--- train.py
+++ train.py
@@ -88,10 +88,11 @@ class Trainer():
                 l=sup_l
             )
 
-            sup_label_a, sup_label_b = label_ids, label_ids[sup_idx]
-            mixed_sup_label = sup_l * sup_label_a + (1 - sup_l) * sup_label_b
+            if cfg.mixup:
+                sup_label_a, sup_label_b = label_ids, label_ids[sup_idx]
+                label_ids = sup_l * sup_label_a + (1 - sup_l) * sup_label_b
 
-            loss = -torch.sum(F.log_softmax(sup_logits, dim=1) * mixed_sup_label, dim=1)
+            loss = -torch.sum(F.log_softmax(sup_logits, dim=1) * label_ids, dim=1)
             loss = torch.mean(loss)
 
             total_train_loss += loss.item()
