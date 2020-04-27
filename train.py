5eb88e0857b4e49bf5c1b5d83fe7f2a7d44d14c9
wancong <wancongzhang@gmail.com>
2020-04-27 10:07:01 -0400

    'd'

--- train.py
+++ train.py
@@ -150,7 +150,7 @@ class Trainer():
 
             # Clip the norm of the gradients to 1.0.
             # This is to help prevent the "exploding gradients" problem.
-            torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
+            #torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
 
             optimizer.step()
 
