e73d6ba86409374783453e42b630700d661b916a
wancong <wancongzhang@gmail.com>
2020-06-09 17:32:03 -0400

    'd'

--- dataset.py
+++ dataset.py
@@ -111,6 +111,10 @@ class DataSet():
         labels = torch.tensor(labels)
         num_tokens = torch.tensor(num_tokens)
 
+        if sentences2:
+            pdb.set_trace()
+
+
         return input_ids, attention_masks, segment_ids, labels, num_tokens
 
     def sample_dataset(self, df, total):
