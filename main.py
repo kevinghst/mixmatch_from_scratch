314f542b7a7ca3ef53fd3eef65e248e480f84ef9
wancong <wancongzhang@gmail.com>
2020-06-14 10:20:36 -0400

    'd'

--- main.py
+++ main.py
@@ -14,7 +14,10 @@ from transformers import RobertaConfig
 
 from models_new import Classifier
 from models import BertForSequenceClassificationCustom, RobertaForSequenceClassificationCustom
-from models_roberta import RobertaForSequenceClassification
+
+#from models_roberta import RobertaForSequenceClassification
+from transformers import RobertaForSequenceClassification
+
 
 from dataset import DataSet
 from train import Trainer
